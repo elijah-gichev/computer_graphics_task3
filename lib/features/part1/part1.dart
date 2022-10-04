d91a3b9260a495e55cfeacc025715bf9980a20ee
elijah gichev <elijah.gichev@gmail.com>
2022-10-04 19:18:09 +0300

    initial commit

---
+++ lib/features/part1/part1.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Part1Page extends StatelessWidget {
+  const Part1Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const SizedBox(),
+    );
+  }
+}
---
+++ lib/features/part2/part2.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Part2Page extends StatelessWidget {
+  const Part2Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const SizedBox(),
+    );
+  }
+}
